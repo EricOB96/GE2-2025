32e2602abac8a79378149af437df2ddb56547ebc
Eric O'Brien <91565520+EricOB96@users.noreply.github.com>
2025-02-11 12:07:01 +0000

    Pursue
    pursue, offset pursue

--- emergence/boid.gd
+++ emergence/boid.gd
@@ -5,14 +5,64 @@ extends CharacterBody3D
 @export var accel:Vector3
 
 @export var mass:float = 1
-var max_speed = 10
+@export var max_speed:float = 10
 
 @export var seek_enabled = false
 @export var arrive_enabled = true
+
 @export var arrive_target:Node3D
 @export var slowing_distance = 20
 
 @export var banking:float = 1
+@export var damping:float = 0.3
+
+@export var player_steering_enabled:bool = true
+@export var s_force:float = 10.0
+
+@export var pursue_enabled = true
+@export var pursue_target:CharacterBody3D
+
+@export var offset_pursue_enabled = true
+@export var leader:CharacterBody3D
+@export var offset:Vector3
+
+
+
+func offset_persure(leader):
+	
+	var world_target = leader.global_transform * offset
+	
+	
+	var dist = (world_target - global_position).length()
+	
+	var time = dist / max_speed
+	
+	var projected = world_target + (leader.velocity * time)
+	
+	return arrive(projected)
+	
+func persure(pursue_target):
+	var dist = (pursue_target.global_position - global_position).length()
+	
+	var time = dist / max_speed
+	
+	var projected = pursue_target.global_position + (pursue_target.velocity * time)
+	
+	return seek(projected)
+
+
+func player_steering():
+	var s = Input.get_axis("move_back", "move_forward")
+	var f:Vector3 = Vector3.ZERO
+	
+	f = global_basis.z * s * s_force
+	
+	var l = Input.get_axis("turn_left", "turn_right")
+	var xz_direction = global_basis.x
+	xz_direction.y = 0
+	f -= xz_direction.x * l * s_force
+	xz_direction.normalized()
+	
 
 func arrive(target) -> Vector3:
 	var to_target = target.global_position - global_position
@@ -29,12 +79,15 @@ func seek(target) -> Vector3:
 	
 
 func _ready() -> void:
-	pass
+	
+	offset = global_position - leader.global_position
+	
 	
 func draw_gizmos():
 	DebugDraw3D.draw_arrow(global_position, global_position + force, Color.AQUAMARINE, 0.1)
 	DebugDraw3D.draw_arrow(global_position, global_position + velocity, Color.CRIMSON, 0.1)
 	DebugDraw3D.draw_sphere(arrive_target.global_position, slowing_distance, Color.BURLYWOOD)
+	DebugDraw2D.set_text("velocity" , velocity)
 
 func calculate():
 	var f:Vector3 = Vector3.ZERO	
@@ -44,6 +97,8 @@ func calculate():
 		f += arrive(arrive_target)
 	if path_follow_enabled:
 		f += follow_path()
+	if player_steering_enabled:
+		f += player_steering()
 	return f
 
 @export var path:Path3D
@@ -74,7 +129,8 @@ func _process(delta: float) -> void:
 		# https://www.cs.toronto.edu/~dt/siggraph97-course/cwr87/
 		var tempUp = transform.basis.y.lerp(Vector3.UP + (accel * banking), delta * 5.0)
 		look_at(global_transform.origin - velocity, tempUp)
-
+		velocity = velocity.limit_length(max_speed)
+		velocity = velocity - (velocity * damping * delta)
 		# look_at(global_position + velocity)
 		# global_position += velocity * delta
 		
